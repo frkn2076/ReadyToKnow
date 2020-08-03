ea71e885e679359431667642259f4d1d5bba1520
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2020-08-03 14:45:14 +0300

    rest api impl.

--- lib/RestAPI.dart
+++ lib/RestAPI.dart
@@ -1,36 +1,42 @@
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<List<Category>> fetchCategories() async {
-  final response = await http.get('http://localhost:8080/category-api/categories');
+class API {
+  static Future<List<Category>> fetchCategories() async {
+    final response =
+        await http.get('http://dummy.restapiexample.com/api/v1/employees');
 
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return json.decode(response.body)['results']
-      .map((data) => Category.fromJson(data))
-      .toList();
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return json
+          .decode(response.body)['data']
+          .map<Category>((data) => Category.fromJson(data))
+          .toList();
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load category');
+    }
   }
 }
 
 class Category {
-  final int id;
+  final String id;
   final String name;
-  final String image;
-  final String info;
+  final String a;
+  final String b;
+  final String c;
 
-  Category({this.id, this.name, this.image, this.info});
+  Category({this.id, this.name, this.a, this.b, this.c});
 
   factory Category.fromJson(Map<String, dynamic> json) {
     return Category(
-      id: json['id'],
-      name: json['name'],
-      image: json['image'],
-      info: json['info']
-    );
+        id: json['id'],
+        name: json['employee_name'],
+        a: json['employee_salary'],
+        b: json['employee_age'],
+        c: json['profile_image']);
   }
 }
